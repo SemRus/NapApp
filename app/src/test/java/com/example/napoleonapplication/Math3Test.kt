9ba94be95b6a1aeb9944a738750bcfdb0640b4af
SemRus <56474530+SemRus@users.noreply.github.com>
2020-03-30 11:50:55 +0500

    Create Math3Test

---
+++ app/src/test/java/com/example/napoleonapplication/Math3Test.kt
@@ -0,0 +1,10 @@
+package com.example.napoleonapplication
+
+import org.junit.Test
+
+class ExampleUnitTest4 {
+    @Test
+    fun addition_fun(){
+        formatPrise(price = 10.0,size = "kg",discount = 10)
+    }
+}
